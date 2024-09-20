a3ec97687cf7be879881a28d9757aa3edfae66a8
Kilian Linux VM <kilian.mader@telekom.de>
2024-09-20 11:21:40 +0200

    add simple unit test for 'auth.go' file to check for valid authorization header

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,25 @@
+package auth
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestGetAPIKey(t *testing.T) {
+	// Test case: No Authorization Header
+	headers := http.Header{}
+	_, err := GetAPIKey(headers)
+	if err == nil {
+		t.Errorf("expected error but got none")
+	}
+
+	// Test case: Valid Authorization Header
+	headers.Set("Authorization", "ApiKey my-api-key")
+	key, err := GetAPIKey(headers)
+	if err != nil {
+		t.Errorf("did not expect an error, but got: %v", err)
+	}
+	if key != "my-api-key" {
+		t.Errorf("expected API key 'my-api-key', but got: %v", key)
+	}
+}
